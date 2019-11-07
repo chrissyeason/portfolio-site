99eab0af27887ef5111a531b776abde0ff7d1cf2
chrissyeason <chrissyeason@gmail.com>
2019-11-06 21:54:16 -0600

    added fetch call to wake up heroku apps

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {Component} from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
 import Navigation from './components/Navigation/Navigation';
@@ -6,17 +6,38 @@ import DoCoolShit from './components/DoCoolShit/DoCoolShit';
 import CakePlease from './components/CakePlease/CakePlease';
 import MeowWoof from './components/MeowWoof/MeowWoof';
 
-function App() {
-  return (
-    <div className="App">
-        <Navigation />
-        <div className="thumbnail-container">        
-          <DoCoolShit />
-          <CakePlease />
-          <MeowWoof />
-        </div>
-    </div>
-  );
+class App extends Component {
+  constructor(){
+    super();
+  }
+  componentDidMount = () => {
+    fetch('http://localhost:3000/projects')
+    .then(res =>res.json())
+    .then(projects=>this.setState({
+      projects: projects
+    }))
+    .catch(err=>console.log(err))
+
+    fetch(`https://meow-woof-app.herokuapp.com/`,
+    {mode: 'no-cors'})
+    .then(res=>console.log('woke up meowwoof'))
+
+    fetch(`https://do-cool-shit.herokuapp.com/`, 
+    {mode: 'no-cors'})
+    .then(res=>console.log('woke up do-cool-shit'))
+  }
+  render(){
+    return (
+      <div className="App">
+          <Navigation />
+          <div className="thumbnail-container">        
+            <DoCoolShit />
+            <CakePlease />
+            <MeowWoof />
+          </div>
+      </div>
+    );
+  }
 }
 
 export default App;
